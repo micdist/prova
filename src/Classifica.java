089b0afcaa93a89aa9f77f0092e9d26d806cf44c
micdist <michedst@gmail.com>
2023-01-09 17:44:39 +0100

    edit Classifica

--- src/Classifica.java
+++ src/Classifica.java
@@ -5,4 +5,15 @@ public class Classifica {
     //maps each team to its points
     Map<String,Integer> mappaClassifica = new HashMap<>();
 
+    public Classifica(Map<String, Integer> mappaClassifica) {
+        this.mappaClassifica = mappaClassifica;
+    }
+
+    public Map<String, Integer> getMappaClassifica() {
+        return mappaClassifica;
+    }
+
+    public void addSquadra(Squadra squadra) {
+        //complete code;
+    }
 }
