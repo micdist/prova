2df3487dd4eca7caad528b44d751a5992a1f6424
micdist <michedst@gmail.com>
2023-01-09 14:59:04 +0100

    added branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String who = "boss";
+        String who = "";
         System.out.println("Hello world! @" + who);
     }
 }
